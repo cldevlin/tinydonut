d00d8280a5d162d4394f58cdfb72f5409be920c9
Vitaly Zaff <vitalyzavgorodnev@gmail.com>
2021-05-31 15:45:54 +0000

    created menu page template

---
+++ routes/menu.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    db.query(`SELECT * FROM donuts;`)
+      .then(data => {
+        const donuts = data.rows;
+        // res.json({ donuts });
+        const templateVars = { donuts };
+        res.render('menu', templateVars);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+};
