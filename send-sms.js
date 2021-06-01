841a3ee58be081f1ecf3e1c3e985cf5ef5c29018
Vitaly Zaff <vitalyzavgorodnev@gmail.com>
2021-06-01 22:56:39 +0000

    full twilio functionality. Restaurant recieves the order --> specifies time ---> customer gets a message

--- send-sms.js
+++ send-sms.js
@@ -1,19 +1,17 @@
-// const accountSid = 'ACfd94ea29d362f69c15606f58f76b80be';
-// const authToken = '9aec714628d95462ff774be8f0b12800';
-// require('dotenv').config();
-// const client = require('twilio')(accountSid, authToken);
 require('dotenv').config();
 const client = require('twilio')(process.env.ACCOUNTSID, process.env.AUTHTOKEN);
 
-const sendToRestaurant = () => {
-  client.messages.create({
-    to: '+12899903232',
+const sendToRestaurant = (number, name, donut, quantity) => {
+  console.log('TEST!!!!!!!!', number);
+  return client.messages.create({
+    to: `+1${number}`,
     from: '+16474931524',
-    body: `This is a TEST`
+    body: `Order for ${name}: ${quantity} --- ${donut}`
   });
 };
+// sendToRestaurant();
 
-sendToRestaurant();
+module.exports = sendToRestaurant;
 
 
 
