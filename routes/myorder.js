aa2319217d34ce75ee7654e706188be90cfeca0b
Vitaly Zaff <vitalyzavgorodnev@gmail.com>
2021-06-02 16:21:01 +0000

    loop through data.rows to display whole order in sms body

--- routes/myorder.js
+++ routes/myorder.js
@@ -19,7 +19,8 @@ module.exports = (db) => {
   let reply;
   router.post('/', twilio.webhook({validate: false}), function(req, res) {
     reply = req.body.Body;
-    db.query(`SELECT phone FROM users WHERE id=17;`)
+    console.log(reply);
+    db.query(`SELECT phone FROM users WHERE id=18;`)
       .then(data => {
         sendReply(reply, data.rows[0].phone);
       });
@@ -28,8 +29,12 @@ module.exports = (db) => {
   router.get("/", (req, res) => {
     db.query(`SELECT donuts.name AS donut, users.name AS user, order_items.quantity FROM donuts JOIN order_items ON donuts.id= order_items.donut_id JOIN orders ON orders.id = order_items.order_id JOIN users ON orders.user_id = users.id WHERE orders.id = 5;`)
       .then(data => {
-        console.log(data.rows[0].user);
-        sendToRestaurant(2899903232, data.rows[0].user, data.rows[0].donut, data.rows[0].quantity)
+        console.log(data.rows);
+        let sms = '';
+        for (let i of data.rows) {
+          sms += (`${i.donut} -> ${i.quantity} \n`);
+        }
+        sendToRestaurant(2899903232, data.rows[0].user, sms)
           .then(message => {
             const templateVars = { message, user: req.session.user_id };
             res.render('myorder', templateVars);
--- send-sms.js
+++ send-sms.js
@@ -1,12 +1,11 @@
 require('dotenv').config();
 const client = require('twilio')(process.env.ACCOUNTSID, process.env.AUTHTOKEN);
 
-const sendToRestaurant = (number, name, donut, quantity) => {
-  console.log('TEST!!!!!!!!', number);
+const sendToRestaurant = (number, name, sms) => {
   return client.messages.create({
     to: `+1${number}`,
     from: '+16474931524',
-    body: `Order for ${name}: ${quantity} --- ${donut}`
+    body: `Order for ${name}: ${sms}`
   });
 };
 // sendToRestaurant();
