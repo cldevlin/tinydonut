d04ecfc3cafeeaced5da509bd85b492e1112666b
Vitaly Zaff <vitalyzavgorodnev@gmail.com>
2021-06-02 22:17:34 +0000

    fixed bug when myorder page was refreshed it was sending sms everytime

--- routes/myorder.js
+++ routes/myorder.js
@@ -27,10 +27,6 @@ module.exports = (db) => {
   let reply;
   router.post('/', twilio.webhook({ validate: false }), function (req, res) {
     reply = req.body.Body;
-    // if (req.body.Body) {
-    //   reply = req.body.Body;
-    //   req.session.reply = { reply };
-    // }
     db.query(`UPDATE orders SET order_status=1, waiting_time=${reply} WHERE id=4;`);
     db.query(`SELECT phone FROM users WHERE id=16;`)
       .then(data => {
@@ -44,20 +40,11 @@ module.exports = (db) => {
   router.get("/", (req, res) => {
     db.query(`SELECT donuts.name AS donut, users.name AS user, order_items.quantity, orders.order_status, orders.waiting_time FROM donuts JOIN order_items ON donuts.id= order_items.donut_id JOIN orders ON orders.id = order_items.order_id JOIN users ON orders.user_id = users.id WHERE orders.id = 4;`)
       .then(data => {
-        console.log(data.rows);
-        let sms = '';
-        for (let i of data.rows) {
-          sms += (`${i.donut} -> ${i.quantity} \n`);
-        }
-        sendToRestaurant(2899903232, data.rows[0].user, sms)
-          .then(message => {
-            const templateVars = { message, user: req.session.user_id, waiting_time: data.rows[0].waiting_time, status: data.rows[0].order_status };
-            res.render('myorder', templateVars);
-          });
-
-
+        const templateVars = {user: req.session.user_id, waiting_time: data.rows[0].waiting_time, status: data.rows[0].order_status };
+        res.render('myorder', templateVars);
       });
-
   });
   return router;
 };
+
+
